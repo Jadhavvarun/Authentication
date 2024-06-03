3c4b26c9fe17b4259c63632610dade04776ab4b4
Jadhavvarun <jadhavvarun67@gmail.com>
2024-06-03 14:29:20 +0530

    first push

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+
+  onSubmit(form: NgForm) {
+    // Handle form submission logic here
+    console.log('Name:', form.value.name);
+    console.log('Email:', form.value.email);
+    console.log('Password:', form.value.password);
+
+    // Reset the form after submission
+    form.resetForm();
+  }
+}
