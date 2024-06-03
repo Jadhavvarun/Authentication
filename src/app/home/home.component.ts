6dee6276d2570b021074e7746d14bdc4e78bea8d
Jadhavvarun <jadhavvarun67@gmail.com>
2024-06-03 21:54:03 +0530

    todos-api-integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule], 
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  todos: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchTodos();
+  }
+
+  fetchTodos() {
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/todos')
+      .subscribe(
+        (data) => {
+          this.todos = data;
+          console.log(this.todos);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
 }
