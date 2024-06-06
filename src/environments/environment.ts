a8cfe8638890e12d8b85a32202aee14749485122
Jadhavvarun <jadhavvarun67@gmail.com>
2024-06-06 15:56:30 +0530

    link pages

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+import { firebase } from "firebaseui-angular";
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBxX_gmXtXPE-Uw7P__xQugolagz8G2BtQ",
+        authDomain: "angular-firebase-auth-f4ed0.firebaseapp.com",
+        projectId: "angular-firebase-auth-f4ed0",
+        storageBucket: "angular-firebase-auth-f4ed0.appspot.com",
+        messagingSenderId: "843020352072",
+        appId: "1:843020352072:web:e1cd282deb72e5669af67d"
+    }
+};
